b60244c8d72944e5656186b868e65d164bed62f1
Ajeetv21 <ajeetv628@gmail.com>
2024-03-01 10:53:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './Component/Sidebar'
+
+function App() {
+  
+  return (
+    <main className='flex'>
+      <Sidebar/>
+      <div>
+        Component
+      </div>
+    </main>
+  )
+}
+
+export default App
